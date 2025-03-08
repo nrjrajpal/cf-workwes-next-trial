478d894db981cae8e9e8724fb094f9904b64dd55
nrjrajpal <nrjrpl007@gmail.com>
2025-03-08 15:01:17 +0530

    temp commit2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="h-screen w-screen bg-lime-200">
-      <h1 className="text-6xl">Hello World!</h1>
+      <h1 className="text-6xl">Hello World!!</h1>
     </div>
   );
 }
